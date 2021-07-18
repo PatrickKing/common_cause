4397493cbf9fac3a88fe570f2ebada9cb56d9e2e
Patrick King <patrick.f.king@gmail.com>
2021-07-18 17:13:32 -0600

    Scaffold User.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'core_pages#public_home'
@@ -7,18 +9,24 @@ Rails.application.routes.draw do
 
   get '/admin', to: 'admin_pages#admin_home'
 
+  scope 'admin', as: :admin do
+    get 'posts', to: 'admin_pages#posts'
+    get 'files', to: 'admin_pages#files'
+    get 'tags', to: 'admin_pages#tags'
+    get 'settings', to: 'admin_pages#settings'
+    get 'users', to: 'admin_pages#users'
+    get 'menu_items', to: 'admin_pages#menu_items'
 
-  get '/admin/posts', to: 'admin_pages#posts'
-  get '/admin/files', to: 'admin_pages#files'
-  get '/admin/tags', to: 'admin_pages#tags'
-  get '/admin/settings', to: 'admin_pages#settings'
-  get '/admin/users', to: 'admin_pages#users'
-  get '/admin/menu_items', to: 'admin_pages#menu_items'
+    scope 'settings', as: :settings do
+      get 'site_identity', to: 'admin_pages#site_identity'
+      get 'podcast_feed', to: 'admin_pages#podcast_feed'
+      get 'twitter', to: 'admin_pages#twitter'
+      get 'substack', to: 'admin_pages#substack'
+    end
+    
+  end
 
-  get '/admin/settings/site_identity', to: 'admin_pages#site_identity'
-  get '/admin/settings/podcast_feed', to: 'admin_pages#podcast_feed'
-  get '/admin/settings/twitter', to: 'admin_pages#twitter'
-  get '/admin/settings/substack', to: 'admin_pages#substack'
+
 
 
 
